3fdae3df55293c4bbf16c5450f2698614151c90c
OsamaBinladder <es1188mr@gmail.com>
2022-04-12 09:32:54 +0300

    hhu

--- calculator.py
+++ calculator.py
@@ -1,25 +1,44 @@
+from glob import glob
 from tkinter import*
+
+from setuptools import Command
 calcbody=Tk()
 calcbody.title("calculator")
 #calcbody.geometry("500x600")
 
-poga0=Button(calcbody,text="0",bg="black",fg="lightgreen",padx="40",pady="20")
-poga1=Button(calcbody,text="1",bg="black",fg="lightgreen",padx="40",pady="20")
-poga2=Button(calcbody,text="2",bg="black",fg="lightgreen",padx="40",pady="20")
-poga3=Button(calcbody,text="3",bg="black",fg="lightgreen",padx="40",pady="20")
-poga4=Button(calcbody,text="4",bg="black",fg="lightgreen",padx="40",pady="20")
-poga5=Button(calcbody,text="5",bg="black",fg="lightgreen",padx="40",pady="20")
-poga6=Button(calcbody,text="6",bg="black",fg="lightgreen",padx="40",pady="20")
-poga7=Button(calcbody,text="7",bg="black",fg="lightgreen",padx="40",pady="20")
-poga8=Button(calcbody,text="8",bg="black",fg="lightgreen",padx="40",pady="20")
-poga9=Button(calcbody,text="9",bg="black",fg="lightgreen",padx="40",pady="20")
-pogasum=Button(calcbody,text="+",bg="black",fg="lightgreen",padx="40",pady="20")
-pogac=Button(calcbody,text="C",bg="black",fg="lightgreen",padx="40",pady="20")
-pogaminus=Button(calcbody,text="-",bg="black",fg="lightgreen",padx="40",pady="20")
-pogadal=Button(calcbody,text="/",bg="black",fg="lightgreen",padx="40",pady="20")
-pogareiz=Button(calcbody,text="*",bg="black",fg="lightgreen",padx="40",pady="20")
-pogaplus=Button(calcbody,text="+",bg="black",fg="lightgreen",padx="40",pady="20")
+def clickbtn(num):
+    current=e.get()#nolasa skaitli
+    e.delete(0,END)#nodzēš
+    newNumber=str(current)+str(num)
+    e.insert(0,newNumber)# ievieto disp
+    return 0
 
+def cmdbtn(command):
+    global num1#iegaume sk un darb
+    global mathOp #mat operators
+    mathOp=command # darbibas
+    num1=int(e.get())
+    e.delete(0,END)
+    return 0
+e=Entry(calcbody,width=20,font=("Arial",26),bg="black",fg="lightgreen")
+poga0=Button(calcbody,text="0",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(0))
+poga1=Button(calcbody,text="1",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(1))
+poga2=Button(calcbody,text="2",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(2))
+poga3=Button(calcbody,text="3",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(3))
+poga4=Button(calcbody,text="4",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(4))
+poga5=Button(calcbody,text="5",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(5))
+poga6=Button(calcbody,text="6",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(6))
+poga7=Button(calcbody,text="7",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(7))
+poga8=Button(calcbody,text="8",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(8))
+poga9=Button(calcbody,text="9",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:clickbtn(9))
+pogasum=Button(calcbody,text="=",bg="black",fg="lightgreen",padx="40",pady="20")
+pogac=Button(calcbody,text="C",bg="black",fg="lightgreen",padx="40",pady="20")
+pogaminus=Button(calcbody,text="-",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:cmdbtn("-"))
+pogadal=Button(calcbody,text="/",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:cmdbtn("/"))
+pogareiz=Button(calcbody,text="*",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:cmdbtn("*"))
+pogaplus=Button(calcbody,text="+",bg="black",fg="lightgreen",padx="40",pady="20",command=lambda:cmdbtn("+"))
+
+e.grid(row=0,column=0,columnspan=5)
 pogaminus.grid(row=4,column=4)
 pogadal.grid(row=3,column=4)
 pogareiz.grid(row=2,column=4)
